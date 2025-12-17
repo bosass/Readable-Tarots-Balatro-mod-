696404060dec0c3f0401e8ab0f358bf782841b91
B <81105398+bosass@users.noreply.github.com>
2025-12-17 04:50:08 +0100

    yes

---
+++ Readable Tarots/main.lua
@@ -0,0 +1,24 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Readable Tarots
+--- MOD_ID: readable_tarots
+--- PREFIX: sas
+--- MOD_AUTHOR: [sas]
+--- MOD_DESCRIPTION: Easier to understand at a glance tarots
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+AltTexture({
+    key = 'sas_texture',
+    set = 'Tarot',
+    path = 'Tarots.png',
+})
+TexturePack({
+    key = 'sas_pack',
+    textures = {
+        'sas_texture',
+    },
+    loc_txt = {
+    name = 'Readable Tarots',
+    text = {'Easier to understand', 'at a glance tarots'}
+    }
+})
